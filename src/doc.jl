1ea672835d2f81309eb2508475f4f99c091f87e6
Nicholas Bauer <nicholasbauer@outlook.com>
2021-08-17 16:49:05 -0400

    Revamped doc function based on new understanding

--- src/doc.jl
+++ src/doc.jl
@@ -1,125 +1,114 @@
+mutable struct Channel
+    coordinates
+    Lr
+    allrelativedensity
+    abovethreshold
+    docscores
+end
+
+
 # calculate degree of colocalization (DoC) scores
-function doc!(channels, Lr_radius, radiusmax, step, roiarea)
-    println("Segment clustered points from background...")
+function doc!(channels, localradius, radiusmax, step, roiarea)
+    0 < localradius < radiusmax ||
+        throw(ArgumentError("$(:localradius) must be positive and less than $(:radiusmax); got $localradius, $radiusmax"))
+    π * radiusmax ^ 2 ≤ roiarea ||
+        throw(ArgumentError("$(:radiusmax) must describe a circle with area smaller than $(:roiarea); got $radiusmax"))
+
+    #=
+    The algorithm for coordinate-based colocalization (10.1007/s00418-011-0880-5) is:
+    1. For each localization, count the number of localizations (other than itself) within a given radius for each channel.
+    2. Divide by the area examined to get the density for each radius and each point.
+    3. For each point, divide by the density within the maximum radius for each point.
+    4. Steps 1-3 simplify to dividing the number observed in a given radius for each point, divided by the number
+       observed within the maximum radius for each point, multiplied by the maximum radius divided by the given radius.
+    5. Spearman's rank correlation coefficient is calculated between the self-self neighbor distribution and the self-other
+       neighbor distribution.
     
+    The ClusDoC algorithm has an initial threshold step to decide which points to include in the calculation.
+    However, it seems to be broken; either the Lr calculation is producing too-high values, or the threshold is too low.
+    Essentially, all points that have any neighbors are above the threshold. I think what it is supposed to be is a calculation
+    of the number of points expected to be within Lr_radius of a point assuming all were evenly distributed across the ROI. And the
+    Lr for a single point would then just be the number of neighbors within Lr_radius of it. But the Lr calculation appears instead to
+    calculate the radius of a circle in which that fraction of points would be expected to appear, with the ROI area doubled. (holdover
+    from a previous iteration in which this value was the side of the ROI square?)
+
+    I *think* then that the threshold is really just the Lr_radius. Which is what the original code complained about.
+    =#
+
+
+
+    println("Segment clustered points from background...")
+
     # This threshold was apparently simplified from a more convoluted one which
     # a comment said basically equalled Lr_radius
     # Lr_Threshold should be number of points within Lr_r for a random
     # distrubution of the same number of points in the current ROI
-    Lr_threshold = length(allcoordinates) * π * (Lr_radius ^ 2) / roiarea; # original divided by ROI size
-    
+
+
+    # The threshold is the number of points that would fall within Lr_radius if randomly distributed over the ROI area.
+    # originally, this value was treated the same as the Lr function does.
     allcoordinates = [c.coordinates for c ∈ channels]
+    
     allneighbortree = BallTree(allcoordinates) # original uses KDTree, should compare
     ctrees = BallTree.(c.coordinates for c ∈ channels)
+    radiussteps = (1:ceil(radiusmax / step)) .* step
     for (i, c) ∈ enumerate(channels)
-        c.Lr, c.allrelativedensity = lr(allneighbortree, c.coordinates, lr_radius)
-        _, c.relativedensity = lr(ctrees[i], c.coordinates, lr_radius)
-        c.abovethreshold = Lr .> Lr_threshold
+        ineighbors = inrange(allneighbortree, c.coordinates, radius, true)
+        nneighbors = length.(ineighbors) .- 1 # remove self
+        ntotal = length(allneighbortree) - 1 # remove self
+        c.equivalentradius = equivalentradius.(nneighbors, ntotal, roiarea)
+        c.density = density.(nneighbors, localradius)
+        c.abovethreshold = c.equivalentradius .> localradius # maybe can replace with simple number threshold though?
+
+        distributions = Vector(undef, length(channels))
+        for j ∈ eachindex(channels)
+            k = Int(i == j) # factor to remove self
+            dr = [(length.(inrange(ctrees[j], c.coordinates, r)) .- k) ./ r ^ 2 for r ∈ radiussteps]
+            distributions[j] = dr ./ dr[end]
+        end
+
+        for j ∈ eachindex(channels)
+            spearmancoefficient = corspearman.(c.distributions[i], distributions[j])
+            _, nearestdistance = nn.(ctrees[j], c.coordinates)
+            c.docscore = spearmancoefficient .* exp(-nearestdistance / radiussteps[end])
+        end
     end
-    
-    # assumes two channels
-    # not sure that these denominators are all correct, but they are what the original code used...
-    D1max = count(channels[1].abovethreshold) / roiarea ^ 2 # should it be squared?
-    D2max = count(channels[2].abovethreshold) / roiarea ^ 2
-    D2maxCh2Ch1 = length.(first(lr(channels[2].coordinates, channels[1].coordinates, radiusmax))) / radiusmax ^ 2 # check for Ch2 points that are within radiusmax of each Ch1 point
-
-    # original starts a stopwatch index here
-
-    println("Calculating DoC scores...")
-
-    # original does this in a parallel loop
-    nsteps = ceil(radiusmax / step)
-    N11 = Vector(undef, nsteps)
-    N12 = Vector(undef, nsteps)
-    N22 = Vector(undef, nsteps)
-    N21 = Vector(undef, nsteps)
-    for (i, r) ∈ enumerate((1:nsteps) .* step)
-        r2 = r ^ 2
-        N11[i] = inrange(ctrees[1], c[1].coordinates, r) .- 1 ./ (D1max * r2)
-        N12[i] = inrange(ctrees[2], c[1].coordinates, r) ./ (D2max * r2)
-        N22[i] = inrange(ctrees[2], c[2].coordinates, r) .- 1 ./ (D1max * r2)
-        N21[i] = inrange(ctrees[1], c[2].coordinates, r) ./ (D2maxCh2Ch1 * r2)
-    end
-
-    println("Correlating coefficients...")
-
-    SA1 = corspearman.(N11, N12)
-    SA2 = corspearman.(N22, N21)
 
     # original sets any NaNs in SA1/2 to 0
-
-    # DoC Assignment
-    # finds distance to the nearest neighbor between each channel
-    _, nearestdistance1 = nn(ctrees[2], channels[1].coordinates)
-    _, nearestdistance2 = nn(ctrees[2], channels[2].coordinates)
-
-    # DoC is the correlation score multiplied by an exponential that scales with the distance to the nearest neighbor as a fraction of radiusmax
-    channels[1].doc = SA1 .* exp(-nearestdistance1 / radiusmax)
-    channels[2].doc = SA2 .* exp(-nearestdistance2 / radiusmax)
 
     return channels
     # original ends a stopwatch here
 end
 
-lr(coordinates1::AbstractVector, coordinates2::AbstractVector, radius) = lr(BallTree(coordinates1), coordinates2, radius)
-function lr(neighbortree::NNTree, coordinates2::AbstractVector, radius, roiarea)
-    ineighbors = inrange(neighbortree, coordinates2, radius, true)
-    kfuncans = length.(ineighbors)
-    density = length.(ineighbors) ./ (π * radius ^ 2)
+"""
+    density(nneighbors::Int, radius)
 
-    Lr = ((roiarea ^ 2) * kfuncans / (length(coordinates2) - 1) / π) .^ 0.5 # numerator multiplied by the area of the ROI
-
-    return Lr, density # original returns Lr, indexes, displacement, and density
+Calculate the density of points in a circle of a given radius.
+"""
+function density(nneighbors::Int, radius)
+    nneighbors ≥ 0 ||
+        throw(ArgumentError("$(:nneighbors) must be greater than equal to zero; got $nneighbors"))
+    radius > 0 ||
+        throw(ArgumentError("$(:radius) must be positive; got $radius"))
+    return nneighbors / (π * radius ^ 2)
 end
-
-
-
-
-
-function doc()
-
-    # filter out those points with poor surrounding distributions
-    allcoordinates = [c.coordinates for c ∈ channels]
-    allneighbortree = BallTree(allcoordinates) # original uses KDTree, should compare
-    for c ∈ channels
-        n_r = number_r(allneighbortree, c.coordinates, lr_radius, true)
-        c.abovethreshold = n_r .> Lr_threshold
-    end
-
-    ch1tree = BallTree(channels[1].coordinates)
-    ch2tree = BallTree(channels[2].coordinates)
-
-    radii = (0:step:radiusmax) .+ step
-    n11_r = number_r.(Ref(channels[1].coordinates), ch1tree, radii, true)
-    n12_r = number_r.(Ref(channels[1].coordinates), ch2tree, radii, false)
-    n22_r = number_r.(Ref(channels[2].coordinates), ch2tree, radii, true)
-    n21_r = number_r.(Ref(channels[2].coordinates), ch1tree, radii, false)
-
-    radiusfactors = radiusmax ^ 2 ./ radii .^ 2
-    distribution11_r = (n11_r ./ n1_rmax) .* radiusfactors
-    distribution12_r = (n12_r ./ n1_rmax) .* radiusfactors
-    distribution22_r = (n22_r ./ n2_rmax) .* radiusfactors
-    distribution21_r = (n21_r ./ n2_rmax) .* radiusfactors
-
-    SA1 = corspearman.(N11, N12)
-    SA2 = corspearman.(N22, N21)
-end
-
-
 
 """
-    distribution_r
+    equivalentradius(nneighbors::Int, ntotal::Int, roiarea)
 
-Calculate the distribution of points in `coordinates_to` around each point in `coordinates_from` for each radius in `radii`.
+Calculate the radius of a circle that would contain the given number of neighbors, assuming the neighbors
+were evenly distributed across the ROI.
 """
-function number_r(coordinates_from::AbstractVector, coordinates_to_tree::NNTree, radius, remove_self::Bool)
-    number_r = inrange(coordinates_to_tree, coordinates_from, radius)
-    if length(coordinates_from > 0) && remove_self
-        number_r .-= 1
-    end
-    distribution_r = (number_r ./ number_r[end]) .* (radii[end] ^ 2 / radii .^ 2)
+function equivalentradius(nneighbors::Int, ntotal::Int, roiarea) #Lr
+    0 < ntotal ||
+        throw(ArgumentError("$(:ntotal) must be positive; got $notal"))
+    0 ≤ nneighbors ≤ ntotal ||
+        throw(ArgumentError("$(:nneighbors) and $(:ntotal) must be greater than or equal to zero, and $(:ntotal) must be greater \
+                             than or equal to $(:nneighbors); got $nneighbors, $ntotal"))
+    roiarea > 0 ||
+        throw(ArgumentError("$(:roiarea) must be positive; got $roiarea"))
+    fneighbors = nneighbors / ntotal
+    equivalentarea = fneighbors * roiarea
+    equivalentradius = sqrt(equivalentarea / π)
+    return equivalentradius
 end
-
-
-
-
