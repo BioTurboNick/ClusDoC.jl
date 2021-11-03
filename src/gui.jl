5052531cfc5249edcc12345eb56701a15029a861
Nicholas Bauer <nicholasbauer@outlook.com>
2021-11-03 16:52:50 -0400

    Improvements to GUI

--- src/gui.jl
+++ src/gui.jl
@@ -33,13 +33,30 @@ end
 
 outputfolder = Observable("")
 on(button(widget = b["outputbtn"])) do _
-    outputfolder[] = pick_folder()
+    outputfolder[] = joinpath(pick_folder(), "ClusDoC Results")
 end
 textbox(String; widget = b["outputtxt"], observable = outputfolder)
 
-selectedfile = Observable("")
+selectedfile = Observable{Union{Nothing, String}}(nothing)
+fileselector = dropdown([], widget = b["fileselector"])
 on(inputfiles) do obs
-    dropdown(basename.(obs), widget = b["fileselector"], observable = selectedfile)
+    @idle_add begin
+        empty!(fileselector)
+        append!(fileselector, basename.(obs))
+        fileselector[] = basename(inputfiles[][1])
+        selectedfile[] = inputfiles[][1]
+    end
+end
+on(fileselector) do obs
+    selectedfile[] = obs !== nothing ? inputfiles[][findfirst(x -> basename(x) == obs, inputfiles[])] : nothing
+end
+
+on(selectedfile) do obs
+    obs === nothing && return
+    locs = loadlocalizations(obs, LocalizationMicroscopy.nikonelementstext)
+    ch1locs = getlocalizations(locs, "488", 1, 11000, 100, 10)
+    ch2locs = getlocalizations(locs, "647", 11001, 11000, 100, 10)
+    
 end
 
 
@@ -50,3 +67,43 @@ add_button!(filebox2, "Input ROIs", () -> push!(inputrois, pick_file()))
 
 is_initializing = false
 Gtk.showall(win)
+
+
+
+win = Window("Gtk") |> (bx = Box(:v))
+lb = Label("(-, -)")
+canvas = Canvas(600, 450)
+push!(bx, lb, canvas)
+@guarded function draw(widget)
+    ctx = Gtk.getgc(widget)
+    w = Gtk.width(widget)
+    h = Gtk.height(widget)
+    #ENV["GKS_WSTYPE"] = "142"
+    #ENV["GKSconid"] = @sprintf("%lu", UInt64(ctx.ptr))
+    Gtk.rectangle(ctx, 0, 0, w, h)
+    Gtk.set_source_rgb(ctx, 1, 0.5, 1)
+    Gtk.fill(ctx)
+    plot([1, 2, 3])
+end
+
+canvas.draw = draw
+
+Gtk.showall(win)
+
+
+win = Window("Gtk") |> (bx = Box(:v))
+c = Canvas(600, 450)
+push!(bx, c)
+@guarded draw(c) do widget
+    ctx = getgc(c)
+    # ENV["GKS_WSTYPE"] = "142"
+    # ENV["GKSconid"] = @sprintf("%lu", UInt64(ctx.ptr))
+    h = height(c)
+    w = width(c)
+    Gtk.rectangle(ctx, 0, 0, w, h)
+    Gtk.set_source_rgb(ctx, 1, 0.5, 1)
+    Gtk.fill(ctx)
+    #plot([1, 2, 3], size=(h, w))
+end
+c.draw = draw
+showall(win)
