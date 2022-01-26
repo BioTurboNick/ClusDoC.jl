f86dee64f8be7d47ca16237bbb63772c8d9f2919
Nicholas Bauer <nicholasbauer@outlook.com>
2022-01-26 12:08:46 -0500

    Fix doc maps

--- src/output.jl
+++ src/output.jl
@@ -30,7 +30,8 @@ function generate_doc_maps(cr::Vector{ChannelResult}, outputpath, filename, i, c
     for j ∈ eachindex(cr)
         for k ∈ eachindex(cr)
             k != j || continue
-            scatter(cr[j].coordinates[1, cr[j].pointdata.abovethreshold], cr[j].coordinates[2, cr[j].pointdata.abovethreshold], markerz = cr[j].pointdata[!, Symbol(:docscore, k)], markersize = 4, alpha = 0.5, markerstrokewidth = 0, seriescolor = :balance, clims = (-1, 1), tickfontsize = 24)
+            abovethreshold = cr[j].pointdata.abovethreshold
+            scatter(cr[j].coordinates[1, abovethreshold], cr[j].coordinates[2, abovethreshold], markerz = cr[j].pointdata[abovethreshold, Symbol(:docscore, k)], markersize = 4, markerstrokewidth = 0, seriescolor = :balance, clims = (-1, 1), tickfontsize = 24)
             plot!(size=(2048,2048), legend = :none, aspectratio = :equal, axis = false, ticks = false, xlims = (xmin, xmax), ylims = (ymin, ymax))
             path = joinpath(outputpath, "doc maps")
             mkpath(path)
