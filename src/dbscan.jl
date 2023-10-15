c6fbfc94bf8db5b47190a032e0b3f5cefe23d0db
Nicholas Bauer <nicholasbauer@outlook.com>
2023-10-15 10:25:41 -0400

    I don't think calculating the thresholds is needed in dbscan anymore?

--- src/dbscan.jl
+++ src/dbscan.jl
@@ -1,14 +1,5 @@
-# if channelresult already has abovethreshold set, it'll use that instead of recalculating.
-
 function dbscan!(channels::Vector{ChannelResult}, clusterparameters, localradius)
     for (i, c) ∈ enumerate(channels)
-        if clusterparameters[i].uselocalradius_threshold && isnothing(c.pointdata.abovethreshold)
-            allcoordinates = reduce(hcat, c.coordinates for c ∈ channels)
-            allneighbortree = BallTree(allcoordinates)
-            nneighbors = inrangecount(allneighbortree, c.coordinates, localradius, true)
-            equivalentradii = equivalentradius.(nneighbors, ntotal, roiarea)
-            c.pointdata.abovethreshold = equivalentradii .> localradius # maybe can replace with simple number threshold though, if don't need to compare across channels
-        end
         coordinates = clusterparameters[i].uselocalradius_threshold ? c.coordinates[:, c.pointdata.abovethreshold] : c.coordinates
         length(coordinates) > 0 || continue
         clusters = Clustering.dbscan(coordinates, clusterparameters[i].epsilon, min_cluster_size = clusterparameters[i].minpoints)
